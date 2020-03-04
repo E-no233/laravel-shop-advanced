18a144cb85e260a32fd32715eab48f6012982eeb
E-no <632451830@qq.com>
2020-03-04 07:54:25 +0000

    初始化类目模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,15 +5,57 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property string $image
+ * @property bool $on_sale
+ * @property float $rating
+ * @property int $sold_count
+ * @property int $review_count
+ * @property float $price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read mixed $image_url
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\ProductSku[] $skus
+ * @property-read int|null $skus_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereOnSale($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereReviewCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereSoldCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereUpdatedAt($value)
+ * @mixin \Eloquent
+ * @property int|null $category_id
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Product whereCategoryId($value)
+ */
 class Product extends Model
 {
     protected $fillable = [
-        'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price'
+        'title',
+        'description',
+        'image',
+        'on_sale',
+        'rating',
+        'sold_count',
+        'review_count',
+        'price'
     ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
+
     // 与商品SKU关联
     public function skus()
     {
@@ -27,5 +69,10 @@ class Product extends Model
             return $this->attributes['image'];
         }
         return \Storage::disk('public')->url($this->attributes['image']);
+    }
+
+    public function category()
+    {
+        $this->belongsTo(Category::class);
     }
 }
