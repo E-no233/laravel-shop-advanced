8973bca8f87f28fc72d7b6965a98a9991b5adf38
E-no <632451830@qq.com>
2020-03-05 03:30:47 +0000

    顶部导航加入商品类目树

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -78,7 +78,7 @@ class ProductsController extends Controller
         if ($user = $request->user()) {
             // 从当前用户已收藏的商品中搜索 id 为当前商品 id 的商品
             // boolval() 函数用于把值转为布尔值
-            $favored = boolval($user->favoriteProducts()->find($product->id));
+            $favored = (bool)$user->favoriteProducts()->find($product->id);
         }
 
         $reviews = OrderItem::query()
